24b44df8c098206c75c9928ee9a98ed68310fc73
junho <jyheo3@naver.com>
2023-12-12 15:06:22 +0900

    Create loginPage and spring boot fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TopMember from "./components/member/member";
+import Home from "./components/main";
+import LoginPage from "./components/member/loginPage";
+
 
 function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/loginPage" element={<LoginPage />}/>
+            </Routes>
+        </BrowserRouter>
+    );
 
-  return (
-      <main>
-  <TopMember name="login"/>
-  <TopMember name="register"/>
-
-      </main>
-  );
 }
 
 export default App;
---
+++ frontend/src/setUpProxy.js
@@ -0,0 +1,28 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+
+    app.use(
+        '/aa',
+        createProxyMiddleware({
+            target: '/',
+            changeOrigin: true,
+        })
+
+    );
+    app.use(
+        '/spring',
+        createProxyMiddleware({
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        '/django',
+        createProxyMiddleware({
+            target: 'http://localhost:8321',
+            changeOrigin: true,
+        })
+    );
+};
