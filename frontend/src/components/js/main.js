a3a2158f56d69ef03acca4a637cf21a3b24bdddf
YE-JIN-KIM <yeje104@naver.com>
2023-12-15 09:34:00 +0900

    map 중간 커밋

---
+++ frontend/src/components/js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+	// INITIALIZE DATEPICKER PLUGIN
+	$('.datepicker').datepicker({
+		clearBtn: true,
+		autoclose: true,
+		format: "yyyy-mm-dd"
+	});
+
+	$('#timepicker').timepicker({
+        template: 'dropdown',
+        showInputs: false,
+        minuteStep: 60,
+        showMeridian: false,
+		defaultTime: 'current',
+		disableMousewheel: true,
+		autoclose: true, 
+		icons: {                  
+			up: 'fa fa-chevron-up',
+			down: 'fa fa-chevron-down'
+		},
+    });
+
+	// 시간이 선택되면 값을 인풋에 설정
+	$('#timepicker').on('changeTime.timepicker', function (e) {
+		var pickedTime = e.time.value;
+		$('#timepicker input').val(pickedTime);
+	});
+
+	
+
+});
---
+++ frontend/src/components/map/map.js
@@ -0,0 +1,37 @@
+let express = require("express");
+let axios = require("axios");
+const path = require('path');
+
+let app = express()
+let port = process.env.PORT || 8087;
+
+app.use(express.static("html"));
+app.use(express.static(path.join(__dirname, '../style')));
+app.use(express.static(path.join(__dirname, '../js')));
+
+app.listen(port, function () {
+    console.log("HTML 서버 시작")
+})
+// http://openapi.seoul.go.kr:8088/704270597679656a35366a6f776e72/xml/bikeStationMaster/1/5/
+// http://openapi.seoul.go.kr:8088/6c4c52594d79656a38317a4b684554/json/tbCycleStationInfo/1/5/
+// http://openapi.seoul.go.kr:8088/537676746c79656a39306c4b4a5a64/json/bikeList/1/5/
+app.get("/map_list", async (req, res) => {
+    try {
+        // 페이지네이션을 위한 변수
+        const itemsPerPage = 1000;  // 페이지당 아이템 수
+        const totalItems = 3000;  // 전체 아이템 수
+        let allData = [];
+
+        // 페이지별로 요청하여 데이터를 합침
+        for (let page = 1; page <= Math.ceil(totalItems / itemsPerPage); page++) {
+            let response = await axios.get(`http://openapi.seoul.go.kr:8088/456852427579656a313035727966656c/json/bikeList/${(page - 1) * itemsPerPage + 1}/${page * itemsPerPage}`);
+            allData = allData.concat(response.data.rentBikeStatus.row);
+        }
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json({ rentBikeStatus: { row: allData } });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("내부 서버 오류");
+    }
+});
