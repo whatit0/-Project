7c223bf06a024096f836225500df83fbdce98ad1
YE-JIN-KIM <yeje104@naver.com>
2023-12-16 17:04:39 +0900

    지도 데이터 보내기 전 과정까지 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Map from './components/map2/Map';
+
+
 
 function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Map />} />
+            </Routes>
+        </BrowserRouter>
+    );
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
