f73422300894f0a36b0ad66d0b3d15f62ec43429
gukin <wkekrkqk159@naver.com>
2023-12-21 11:20:14 +0900

    채팅 css 수정

--- frontend/src/components/chat/RoomDetail.jsx
+++ frontend/src/components/chat/RoomDetail.jsx
@@ -1,25 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import Stomp from 'stompjs';
+import React, { useEffect, useRef, useState } from 'react';
 import SockJS from 'sockjs-client';
-// import styles from "../style/room.css";
-import '../style/room.css'
-import '../style/bootstrap.css'
+import Stomp from 'stompjs';
+import '../style/bootstrap.css';
+import '../style/room.css';
 
-const RoomDetail = () => {
+const RoomDetail = ({ roomId }) => {
     const [room, setRoom] = useState({});
     const [name, setName] = useState('');
     const [userId, setUserId] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const roomId = localStorage.getItem('wschat.roomId');
-    const [connected, setConnected] = useState(false); // 연결 상태 추적
+    const [connected, setConnected] = useState(false);
     const stompClient = useRef(null);
-
 
     useEffect(() => {
         // Room 정보 가져오기
-        axios.get('http://localhost:8080/chat/room/' + roomId)
+        axios.get(`http://localhost:8080/chat/room/${roomId}`)
             .then(response => {
                 if (response.data) {
                     setRoom(response.data);
@@ -38,11 +35,9 @@ const RoomDetail = () => {
         const socket = new SockJS('http://localhost:8080/ws-stomp');
         stompClient.current = Stomp.over(socket);
 
-        const token = localStorage.getItem('accessToken'); // 토큰 가져오기
-        stompClient.current.connect({ 'Authorization': `Bearer ${token}` }, (frame) => { // .current를 통해 접근
-            // 연결 성공 시
+        stompClient.current.connect({}, (frame) => {
             setConnected(true);
-            subscribeToRoom();
+            subscribeToRoom(roomId);
 
             if (stompClient.current && stompClient.current.connected) {
                 const enterMessage = {
@@ -54,8 +49,9 @@ const RoomDetail = () => {
                 stompClient.current.send("/pub/chat/message", {}, JSON.stringify(enterMessage));
             }
         }, (error) => {
-            alert("WebSocket 연결 에러 : " + error);
+            alert("WebSocket 연결 에러: " + error);
         });
+
         // 컴포넌트 언마운트 시 WebSocket 연결 종료
         return () => {
             if (stompClient.current && stompClient.current.connected) {
@@ -76,7 +72,7 @@ const RoomDetail = () => {
                 }]);
             });
         }
-    }
+    };
 
     const sendMessage = () => {
         const messageContent = message.trim();
@@ -89,7 +85,6 @@ const RoomDetail = () => {
             };
             stompClient.current.send("/pub/chat/message", {}, JSON.stringify(chatMessage));
             setMessage('');
-
         } else {
             console.log("메시지를 보낼 수 없습니다.");
         }
@@ -97,7 +92,7 @@ const RoomDetail = () => {
 
     const leaveRoom = () => {
         if (stompClient.current && stompClient.current.connected) {
-            stompClient.current.send("/pub/chat/message", {}, JSON.stringify({type:'EXIT', roomId, userid: userId}));
+            stompClient.current.send("/pub/chat/message", {}, JSON.stringify({ type: 'EXIT', roomId, userid: userId }));
             window.location.href = '/chatList';
         } else {
             console.error("웹소켓 연결이 끊어진 상태입니다.");
@@ -122,11 +117,10 @@ const RoomDetail = () => {
         scrollToEnd();
     }, [messages]);
 
-    const d = localStorage.getItem('accessToken');
     return (
-        <div className='container'>
+        <div className='chat_detail_con'>
             <div className="chat-header">
-                <h1>채팅 방 : {name} {d}</h1>
+                <h1>채팅 방: {name}</h1>
                 <div className="buttons">
                     <button className="btn-hover color-11" onClick={leaveRoom}>퇴장</button>
                 </div>
@@ -139,33 +133,43 @@ const RoomDetail = () => {
                     </div>
                 </div>
                 <div className="chatbox__messages">
-                    {messages.map((msg, index) => {
-                        return (
-                            <div key={index} className="chatbox__messages__user-message">
-                                <div className="chatbox__messages__user-message--ind-message">
-                                    <p className="name">{msg.userid}</p>
-                                    <p className="message">{msg.message}</p>
+                    <div className='chatbox__messages__box'>
+                        {messages.map((msg, index) => {
+                            return (
+                                <div key={index} className="chatbox_user_message">
+                                    <div>
+                                        <div className='ind-message-top flex'>
+                                            <p className="name">{msg.userid}예진</p>
+                                            <p className="date">{msg.formattedDate}</p>
+                                        </div>
+                                        <div className="ind-message">
+                                            <p className="message">{msg.message}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <p className="date">{msg.formattedDate}</p>
-                            </div>
-                        );
-                    })}
-                </div>
-                <div className="input-group">
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        placeholder="메시지를 입력하세요"
-                        onKeyDown={handleKeyDown} // 엔터 키 이벤트 핸들러 추가
-                    />
-                    <div className="buttons">
-                        <button className="btn-hover color-1" onClick={sendMessage}>전송</button>
+                            );
+                        })}
                     </div>
+                    <div className="send_form_con flex">
+                        <input
+                            type="text"
+                            className="send_input"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            placeholder="메시지를 입력하세요"
+                            onKeyDown={handleKeyDown} // 엔터 키 이벤트 핸들러 추가
+                        />
+                        <div className="buttons">
+                            <button className="" onClick={sendMessage}>
+                                <span class="material-symbols-rounded white fs30 fw400">near_me</span>
+                            </button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
     );
 };
+
 export default RoomDetail;
--- frontend/src/components/chat/RoomList.jsx
+++ frontend/src/components/chat/RoomList.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // useNavigate 훅 임포트
-import '../style/bootstrap.css'
-
+import Header from '../common/Header';
+import '../style/bootstrap.css';
+import RoomDetail from './RoomDetail';
 
 const RoomList = () => {
     const [rooms, setRooms] = useState([]);
     const [newRoomName, setNewRoomName] = useState('');
+    const [selectedRoom, setSelectedRoom] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -22,68 +24,72 @@ const RoomList = () => {
         }
     };
 
-    const handleCreateRoom = (e) => {
+    const handleCreateRoom = async (e) => {
         e.preventDefault();
-        createRoom();
-    };
-
-    const createRoom = async () => {
         if (newRoomName.trim() === '') return;
+
         try {
             const params = new URLSearchParams();
             params.append('name', newRoomName);
             const response = await axios.post('http://localhost:8080/chat/room', params);
 
-            // 새로운 방 정보를 목록에 추가
             const newRoom = response.data;
             setRooms(prevRooms => [newRoom, ...prevRooms]);
-            localStorage.setItem('wschat.roomId', newRoom.roomId);
-            localStorage.setItem('wschat.roomName', newRoom.name);
-
-            navigate(`/chat/room/enter/${newRoom.roomId}`);
+            setNewRoomName('');
         } catch (error) {
             console.error('Error creating chat room:', error);
         }
     };
 
-    const handleRoomClick = (roomId, roomName) => {
-        localStorage.setItem('wschat.roomId', roomId);
-        localStorage.setItem('wschat.roomName', roomName);
-        navigate(`/chat/room/enter/${roomId}`);
+    const handleRoomClick = async (roomId, roomName) => {
+        setSelectedRoom({ roomId, name: roomName });
     };
 
     return (
-        <div className="container mt-5">
-            <h2>채팅방 목록</h2>
-            <form onSubmit={handleCreateRoom}>
-                <div className="input-group mt-3">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="채팅방 이름 입력"
-                        value={newRoomName}
-                        onChange={(e) => setNewRoomName(e.target.value)}
-                    />
-                    <div className="input-group-append">
-                        <button className="btn-hover color-9" type="submit">방 생성</button>
+        <div id='chat'>
+            <Header />
+            <div className="container flex">
+                <div className='chat_list_con'>
+                    <form onSubmit={handleCreateRoom}>
+                        <div className="input-group input-group-create mt-4 mb-5">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="채팅방 만들기"
+                                value={newRoomName}
+                                onChange={(e) => setNewRoomName(e.target.value)}
+                            />
+                            <button className="create_btn" type="submit">
+                                <span className="material-symbols-rounded white fs25 fw500">add</span>
+                            </button>
+
+                        </div>
+                    </form>
+                    <h2>채팅방 목록</h2>
+                    <div className="list-group mt-3">
+                        {rooms.map(room => (
+                            <div
+                                key={room.roomId}
+                                className="chat_list_group flex"
+                                onClick={() => handleRoomClick(room.roomId, room.name)}
+                            >
+                                <span class="material-symbols-rounded fs25 white sms_icon mr10">sms</span>
+                                <span className="room-name">{room.name}</span>
+                                <button
+                                    className=""
+                                    onClick={() => handleRoomClick(room.roomId, room.name)}
+                                >
+                                    입장<i className="bi bi-emoji-smile"></i>
+                                </button>
+                            </div>
+                        ))}
                     </div>
                 </div>
-            </form>
-            <div className="list-group mt-3">
-                {rooms.map(room => (
-                    <div key={room.roomId}
-                         className="list-group-item d-flex justify-content-between align-items-center">
-                        <span className="room-name">{room.name}</span>
-                        <button
-                            className="btn-hover color-5 btn-enter"
-                            onClick={() => handleRoomClick(room.roomId, room.name)}
-                        >
-                            입장<i className="bi bi-emoji-smile"></i>
-                        </button>
-                    </div>
-                ))}
+                {/* 선택된 방이 있을 때만 RoomDetail을 표시 */}
+                {selectedRoom && <RoomDetail roomId={selectedRoom.roomId} key={selectedRoom.roomId} />}
             </div>
         </div>
     );
 };
+
 export default RoomList;
