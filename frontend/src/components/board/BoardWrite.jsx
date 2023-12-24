fbebbeb3db66674b1ef36084167af19545d52ff7
YE-JIN-KIM <yeje104@naver.com>
2023-12-25 01:43:56 +0900

    Your commit message

--- frontend/src/components/board/BoardWrite.jsx
+++ frontend/src/components/board/BoardWrite.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Header from "../common/Header";
+import "../style/board.css";
 
 function BoardWrite() {
     const token = localStorage.getItem('accessToken');
@@ -39,33 +41,47 @@ function BoardWrite() {
     };
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="boardTitle">제목</label>
-                <input
-                    type="text"
-                    id="boardtitle"
-                    name="boardtitle"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <label htmlFor="boardContent">내용</label>
-                <textarea
-                    id="boardcontent"
-                    name="boardcontent"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                />
-                <label htmlFor="boardfilename">이미지</label>
-                <input
-                    type="file"
-                    id="boardfilename"
-                    name="boardfilename"
-                    onChange={handleFileChange}
-                />
-                <button type="submit">전송</button>
-            </form>
-        </>
+        <div id='board'>
+            <Header />
+            <div className="sub_box">
+                <div className="sub_top sub_write">
+                    <div className="top sub_right_title">
+                        <h2>COMMUNITY</h2>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="flex">
+                            <label htmlFor="boardTitle">제목</label>
+                            <input
+                                type="text"
+                                id="boardtitle"
+                                name="boardtitle"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </div>
+                        <div className="flex">
+                            <label htmlFor="boardContent">내용</label>
+                            <textarea
+                                id="boardcontent"
+                                name="boardcontent"
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)}
+                            />
+                        </div>
+                        <div className="flex">
+                            <label htmlFor="boardfilename">이미지</label>
+                            <input
+                                type="file"
+                                id="boardfilename"
+                                name="boardfilename"
+                                onChange={handleFileChange}
+                            />
+                        </div>
+                        <button type="submit">전송</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
