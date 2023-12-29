cc4aa932c25ee565144f315194606f83f92e03e2
junho <jyheo3@naver.com>
2023-12-29 20:13:28 +0900

    404 에러페이지 임시 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import BoardUpdate from "./components/board/BoardUpdate";
 import Map from "./components/map/Map";
 import Mypage from "./components/mypage/Mypage";
 import Report from "./components/map/Report";
+import ErrorPage from "./components/ErrorPage";
 
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
                 <Route path="/boardUpdate" element={<BoardUpdate />}/>
                 <Route path="/mypage" element={<Mypage />}/>
                 <Route path="/report" element={<Report />}/>
+                <Route path="/*" element={<ErrorPage/>}/>
             </Routes>
         </BrowserRouter>
     );
---
+++ frontend/src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function errorPage(){
+    return (
+        <div style={{ textAlign: 'center', marginTop: '300px' }}>
+            <p style={{fontSize: '50px'}}>죄송합니다. 찾으시는 페이지를 찾을 수 없습니다.</p>
+        </div>
+    );
+}
+
+export default errorPage;
